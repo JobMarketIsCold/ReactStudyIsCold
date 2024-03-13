933e59f5cad73136f3636808d864a29d9f51e960
황현민 <hwanghyunmin0650@gmail.com>
2024-03-13 19:41:10 +0900

    Revert "useState return"
    This reverts commit 34d5002d553fd043848c3abf5a6952dcfbe93dd7.

--- react-is-cold/src/App.js
+++ react-is-cold/src/App.js
@@ -1,20 +1,32 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-	useEffect(() => {
-		console.log("생성 :)");
-		return () => console.log("소멸 :(");
-	});
-	return <h1>HI</h1>;
-}
-
 function App() {
-	const [showing, setShowing] = useState(false);
-	const onClick = () => setShowing((prev) => !prev);
+	const [counter, setValue] = useState(0);
+	const [keyword, setKeyword] = useState("");
+	const onClick = () => setValue((prev) => prev + 1);
+	const onChange = (event) => setKeyword(event.target.value);
+	useEffect(() => {
+		console.log("나는 한 번만 나와!");
+	}, []);
+	useEffect(() => {
+		console.log("나는 키워드가 바뀔 때 작동해");
+	}, [keyword]);
+	useEffect(() => {
+		console.log("나는 카운터가 바뀔 때 작동해");
+	}, [counter]);
+	useEffect(() => {
+		console.log("나는 카운더와 키워드가 바뀔 때 작동해");
+	}, [counter, keyword]);
 	return (
 		<div>
-			{showing ? <Hello /> : null}
-			<button onClick={onClick}>{showing ? "HIDE" : "SHOW"}</button>
+			<input
+				value={keyword}
+				onChange={onChange}
+				type="text"
+				placeholder="검색"
+			/>
+			<h1>Welcome {counter}</h1>
+			<button onClick={onClick}>클릭</button>
 		</div>
 	);
 }
