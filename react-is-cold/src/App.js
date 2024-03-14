325aadbcadf8db1e6469196612fc040cb95778c5
황현민 <hwanghyunmin0650@gmail.com>
2024-03-14 10:35:30 +0900

    TodoList

--- react-is-cold/src/App.js
+++ react-is-cold/src/App.js
@@ -1,20 +1,50 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-	useEffect(() => {
-		console.log("생성 :)");
-		return () => console.log("소멸 :(");
-	});
-	return <h1>HI</h1>;
-}
-
 function App() {
-	const [showing, setShowing] = useState(false);
-	const onClick = () => setShowing((prev) => !prev);
+	const [toDo, setToDo] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		// console.log(toDo);
+		if (toDo === "") return;
+		setToDos((currentArray) => [...currentArray, toDo]);
+		setToDo("");
+	};
+	const deleteTodo = (event, index) => {
+		// const li = event.target.parentElement;
+		setToDos((currentToDos) => {
+			return currentToDos.filter((_, todoIndex) => todoIndex !== index);
+		});
+		// li.remove();
+	};
 	return (
 		<div>
-			{showing ? <Hello /> : null}
-			<button onClick={onClick}>{showing ? "HIDE" : "SHOW"}</button>
+			<h1>나의 ToDos ({toDos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					onChange={onChange}
+					value={toDo}
+					type="text"
+					placeholder="투두를 적으세요"
+				/>
+				<button>Add ToDo</button>
+			</form>
+			<hr />
+			<ul>
+				{toDos.map((item, index) => (
+					<li key={index}>
+						{item}
+						<button
+							onClick={(e) => {
+								deleteTodo(e, index);
+							}}
+						>
+							✖️
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
